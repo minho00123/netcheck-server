a3f6724000e71717af0789719e0d10af357482d8
Min Ho Jang <minho00123@gmail.com>
2024-03-21 00:05:05 +0900

    Feat: Create availability.js

---
+++ src/availability.js
@@ -0,0 +1,17 @@
+const https = require("https");
+
+function getAvailabilityData(url) {
+  return new Promise((resolve, reject) => {
+    const startTime = Date.now();
+
+    https.get(url, res => {
+      const { statusCode } = res;
+      const endTime = Date.now();
+      const responseTime = endTime - startTime;
+
+      resolve({ statusCode, responseTime });
+    });
+  });
+}
+
+module.exports = getAvailabilityData;
