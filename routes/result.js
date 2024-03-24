d3e1c46b07c9e94ed36d06c62f54dff0673d3c20
Min Ho Jang <minho00123@gmail.com>
2024-03-24 20:44:58 +0900

    Refactor: Change result.js in routes

--- routes/result.js
+++ routes/result.js
@@ -1,17 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const {
-  postSpeedData,
-  postSecurityData,
-  postTracerouteData,
-  postInformationData,
-  postReliabilityData,
+  postDataAll,
+  postHistoryData,
+  postHistoryIdData,
 } = require("../controllers/result.controller");
 
-router.post("/result/speed", postSpeedData);
-router.post("/result/security", postSecurityData);
-router.post("/result/traceroute", postTracerouteData);
-router.post("/result/information", postInformationData);
-router.post("/result/reliability", postReliabilityData);
+router.post("/result/all", postDataAll);
+router.post("/history/all", postHistoryData);
+router.post("/history/id", postHistoryIdData);
 
 module.exports = router;
