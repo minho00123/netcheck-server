7d2623bce1f2bb78e7d5f8682dbde8b5971ba051
Min Ho Jang <minho00123@gmail.com>
2024-04-10 12:29:23 +0900

    Refactor: change cors

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const app = express();
 const result = require("./routes/result");
 const share = require("./routes/share");
 
-app.use(cors());
+app.use(cors("https://client.netcheck.site/"));
 app.use(express.json());
 app.use(result);
 app.use(share);
