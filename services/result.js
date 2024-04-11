10658354c317d631d9e6cc0c1fba31ef5368e545
Min Ho Jang <minho00123@gmail.com>
2024-04-11 20:26:41 +0900

    Fix: cors error

--- services/result.js
+++ services/result.js
@@ -31,11 +31,7 @@ exports.processDataAll = async function (req, res) {
   };
 
   const { statusCode, responseTime } = await getAvailabilityData(url);
-  // const { sent, received, lossRate, latencies } = await ping(ipAddress, 10);
-  const sent = 10;
-  const received = 10;
-  const lossRate = 0;
-  const latencies = 100;
+  const { sent, received, lossRate, latencies } = await ping(ipAddress, 10);
   const reliabilityData = {
     statusCode,
     responseTime,
@@ -59,19 +55,19 @@ exports.processDataAll = async function (req, res) {
     };
   }
 
-  // const tracerouteData = await getTracerouteData(url);
-  // const uniqueTracerouteData = extractUniqueTracerouteData(tracerouteData);
+  const tracerouteData = await getTracerouteData(url);
+  const uniqueTracerouteData = extractUniqueTracerouteData(tracerouteData);
 
-  // if (uniqueTracerouteData && uniqueTracerouteData.length > 0) {
-  //   for (const data of uniqueTracerouteData) {
-  //     const response = await axios(`http://ip-api.com/json/${data.ipAddress}`);
+  if (uniqueTracerouteData && uniqueTracerouteData.length > 0) {
+    for (const data of uniqueTracerouteData) {
+      const response = await axios(`http://ip-api.com/json/${data.ipAddress}`);
 
-  //     data.country = response.data.country;
-  //     data.city = response.data.city;
-  //     data.lat = response.data.lat;
-  //     data.lon = response.data.lon;
-  //   }
-  // }
+      data.country = response.data.country;
+      data.city = response.data.city;
+      data.lat = response.data.lat;
+      data.lon = response.data.lon;
+    }
+  }
 
   function extractUniqueTracerouteData(data) {
     const uniqueHops = [];
@@ -86,14 +82,7 @@ exports.processDataAll = async function (req, res) {
 
     return uniqueHops;
   }
-  const tracerouteData = [
-    {
-      country: "South Korea",
-      city: "Seoul",
-      lat: 32,
-      lon: 10,
-    },
-  ];
+
   const result = new Result({
     customId: id,
     url,
